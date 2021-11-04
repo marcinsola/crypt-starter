48129eaf7e96eabaa2d679764bedb7c21d378cb6
Marcin Sola <marcinsolaonline@gmail.com>
2021-11-04 22:16:35 +0100

    Add deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const CryptStarter = await hre.ethers.getContractFactory("CryptStarter");
+  const cryptStarter = await CryptStarter.deploy();
 
-  await greeter.deployed();
+  await cryptStarter.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("CryptStarter deployed to:", cryptStarter.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
