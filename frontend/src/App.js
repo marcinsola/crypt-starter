45e24feddd41b0521ebb4150f326ba8938dba0af
Marcin Sola <marcinsolaonline@gmail.com>
2021-11-17 21:50:20 +0100

    Add container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import Header from './components/Header';
+import { Container } from '@material-ui/core';
+
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Container maxWidth='md'>
+        <h1>Hi!</h1>
+      </Container>
     </div>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import { useEthers } from '@usedapp/core';
+import { Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(4),
+    display: 'flex',
+    justifyContent: 'flex-end',
+    gap: theme.spacing(1),
+  },
+}));
 
 const Header = () => {
+  const classes = useStyles();
   const { account, activateBrowserWallet, deactivate } = useEthers();
   const isConnected = account !== undefined;
   return (
-    <div>
-      {isConnected ? (
-        <button color='primary' onClick={deactivate}>
-          Disconnect
-        </button>
-      ) : (
-        <button color='primary' onClick={activateBrowserWallet}>
-          Connect
-        </button>
-      )}
+    <div className={classes.container}>
+      <div>
+        {isConnected ? (
+          <Button variant='outlined' color='primary' onClick={deactivate}>
+            Disconnect
+          </Button>
+        ) : (
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={activateBrowserWallet}
+          >
+            Connect
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
