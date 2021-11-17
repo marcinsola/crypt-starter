f35675adc06f785f78d3e6ccb5b55afd5556684e
Marcin Sola <marcinsolaonline@gmail.com>
2021-11-17 21:11:45 +0100

    Allow wallet connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from './components/Header';
 function App() {
-  return <div className='App'>Hello!</div>;
+  return (
+    <div className='App'>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
