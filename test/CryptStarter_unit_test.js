d8356b561769272add208036fbab31128a24eb16
Marcin Sola <marcinsolaonline@gmail.com>
2021-11-24 18:33:46 +0100

    Update tests

--- test/CryptStarter_unit_test.js
+++ test/CryptStarter_unit_test.js
@@ -31,21 +31,56 @@ describe('CryptStarter', () => {
       .set({ hour: 0, minute: 0, seconds: 0 });
   });
 
-  it.skip('Campaign cannot be funded if is not in progress', async () => {});
-  it.skip('Campaign owner can claim fund from successful campaign', async () => {});
-  it.skip('Campaign owner cannot claim fund from campaign if status is different than in progress', async () => {});
-  it.skip('Only campaign owner can claim funds');
-  it.skip('Cannot claim funds from non-existent campaign');
-  it.skip('CampaignFundsClaimed emitted upon campaign funding');
+  it('Only campaign owner can claim funds', async () => {
+    await cryptStarter.createCampaign(
+      campaignName,
+      campaignTarget,
+      weekFromNow.unix()
+    );
 
-  it.skip('Backers can withdraw funds from unsuccessful campaign');
-  it.skip('Backers cannot withdraw funds in any other scenario');
-  it.skip('Each backer can withdraw funds only once');
-  it.skip(
-    'UnsuccessfulCampaignFundsWithdrawn emmited upon funds withdrawal by backer'
-  );
+    const [owner, backer1, backer2] = await ethers.getSigners();
+    const numberOfCampaigns = await cryptStarter.getNumberOfCampaigns();
+    const index = numberOfCampaigns - 1;
 
-  it.skip('Shoud fail when deadline is less than 7 days from now', async () => {
+    const amount = ethers.utils.parseEther('0.1');
+    cryptStarter.connect(backer1).fundCampaign(index, { value: amount });
+    cryptStarter.connect(backer2).fundCampaign(index, { value: amount });
+    const campaign = await cryptStarter.campaigns(index);
+    expect(campaign.status).to.equal(0);
+    await expect(
+      cryptStarter.connect(backer2).claimSuccessfulCampaignFunds(index)
+    ).to.be.revertedWith("You're not the author of this campaign");
+  });
+
+  it('Backers cannot withdraw funds in any other scenario', async () => {
+    await cryptStarter.createCampaign(
+      campaignName,
+      campaignTarget,
+      weekFromNow.unix()
+    );
+
+    const [owner, backer1, backer2] = await ethers.getSigners();
+    const numberOfCampaigns = await cryptStarter.getNumberOfCampaigns();
+    const index = numberOfCampaigns - 1;
+
+    const amount = ethers.utils.parseEther('0.1');
+    cryptStarter.connect(backer1).fundCampaign(index, { value: amount });
+    cryptStarter.connect(backer2).fundCampaign(index, { value: amount });
+    const campaign = await cryptStarter.campaigns(index);
+    expect(campaign.status).to.equal(0);
+    await expect(
+      cryptStarter.connect(backer1).withdrawFundsFromUnsuccessfulCampaign(index)
+    ).to.be.revertedWith(
+      'Campaign is either still in progress or had reached it goal'
+    );
+
+    await expect(
+      cryptStarter.connect(backer2).withdrawFundsFromUnsuccessfulCampaign(index)
+    ).to.be.revertedWith(
+      'Campaign is either still in progress or had reached it goal'
+    );
+  });
+  it('Shoud fail when deadline is less than 7 days from now', async () => {
     await expect(
       cryptStarter.createCampaign(
         campaignName,
@@ -62,16 +97,16 @@ describe('CryptStarter', () => {
       weekFromNow.unix()
     );
     let receipt = await transaction.wait();
-    const campaign = await cryptStarter.campaigns(0);
+    const numberOfCampaigns = await cryptStarter.getNumberOfCampaigns();
+    const index = numberOfCampaigns - 1;
     const [campaignCreatedEvent] = receipt.events;
-
     expect(campaignCreatedEvent.event).to.equal('CampaignCreated');
     expect(campaignCreatedEvent.args['owner']).to.equal(signerAddress);
     expect(campaignCreatedEvent.args['name']).to.equal(campaignName);
 
     expect(
       ethers.BigNumber.from(campaignCreatedEvent.args['index']._hex)
-    ).to.equal(0);
+    ).to.equal(index);
 
     expect(
       ethers.BigNumber.from(campaignCreatedEvent.args['target']._hex)
@@ -90,7 +125,7 @@ describe('CryptStarter', () => {
       weekFromNow.unix()
     );
 
-    let campaign = await cryptStarter.campaigns(0);
+    let campaign = await cryptStarter.campaigns(currentNumberOfCampaigns);
     const newNumberOfCampaigns = await cryptStarter.getNumberOfCampaigns();
 
     expect(campaign['owner']).to.equal(signerAddress);
@@ -143,7 +178,6 @@ describe('CryptStarter', () => {
     expect(donation.amount).to.equal(amount);
   });
 
-  it.skip('backersAmounts is calculated correctly');
   it('Campaign cannot be backed by the owner', async () => {
     await cryptStarter.createCampaign(
       campaignName,
@@ -157,4 +191,35 @@ describe('CryptStarter', () => {
       })
     ).to.be.revertedWith('You cannot fund your own campaign');
   });
+
+  it('Campaign owner cannot claim funds from campaign if status is different than successful', async () => {
+    await cryptStarter.createCampaign(
+      campaignName,
+      campaignTarget,
+      weekFromNow.unix()
+    );
+
+    const [owner, backer1, backer2] = await ethers.getSigners();
+    const numberOfCampaigns = await cryptStarter.getNumberOfCampaigns();
+    const index = numberOfCampaigns - 1;
+
+    const amount = ethers.utils.parseEther('0.1');
+    cryptStarter.connect(backer1).fundCampaign(index, { value: amount });
+    cryptStarter.connect(backer2).fundCampaign(index, { value: amount });
+    const campaign = await cryptStarter.campaigns(index);
+    expect(campaign.status).to.equal(0);
+    await expect(
+      cryptStarter.connect(owner).claimSuccessfulCampaignFunds(index)
+    ).to.be.revertedWith('Funds are not ready to withdraw yet');
+  });
+
+  it.skip('CampaignFundsClaimed emitted upon campaign claiming');
+  it.skip('Campaign cannot be funded if is not in progress', async () => {});
+  it.skip('Campaign owner can claim fund from successful campaign', async () => {});
+  it.skip('Backers can withdraw funds from unsuccessful campaign');
+  it.skip('Each backer can withdraw funds only once');
+  it.skip(
+    'UnsuccessfulCampaignFundsWithdrawn emmited upon funds withdrawal by backer'
+  );
+
 });
